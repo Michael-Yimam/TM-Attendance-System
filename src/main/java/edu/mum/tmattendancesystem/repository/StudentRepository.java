7886198428fc4d3a3822a37288ae6e03a5d78208
Fisseha W <fissehaweb@gmail.com>
2019-06-15 11:42:13 -0500

    Rename Student Repository

---
+++ src/main/java/edu/mum/tmattendancesystem/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package edu.mum.tmattendancesystem.repository;
+
+import edu.mum.tmattendancesystem.domain.Student;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends CrudRepository<Student, Long> {
+}
