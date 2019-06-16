a99f2b97b38f33132fada5ef256a0c38a17a5e3e
Michael <mikialebachew@gmail.com>
2019-06-17 03:29:19 +0600

    NoArgs Constructor added

--- src/main/java/edu/mum/tmattendancesystem/domain/Faculty.java
+++ src/main/java/edu/mum/tmattendancesystem/domain/Faculty.java
@@ -1,23 +1,14 @@
 package edu.mum.tmattendancesystem.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
-@Data
 
+@NoArgsConstructor
+@Getter
+@Setter
 @Entity
 public class Faculty extends User{
-
-
-//    @Id
-//    private String facultyId;
-//
-//    private String name;
 
 }
--- src/main/java/edu/mum/tmattendancesystem/domain/Student.java
+++ src/main/java/edu/mum/tmattendancesystem/domain/Student.java
@@ -1,11 +1,20 @@
 package edu.mum.tmattendancesystem.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import java.util.List;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
 @Entity
 public class Student extends User{
 
