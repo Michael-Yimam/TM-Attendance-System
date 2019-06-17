29ece80bcb6b3d12bef601e89c87f1e7685d4502
Fisseha W <fissehaweb@gmail.com>
2019-06-17 10:17:02 -0500

    Project Redesign

---
+++ src/main/java/edu/mum/tmattendancesystem/repository/CourseOfferRepository.java
@@ -0,0 +1,8 @@
+package edu.mum.tmattendancesystem.repository;
+
+import edu.mum.tmattendancesystem.domain.CourseOffer;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CourseOfferRepository extends CrudRepository<CourseOffer, String>{
+
+}
