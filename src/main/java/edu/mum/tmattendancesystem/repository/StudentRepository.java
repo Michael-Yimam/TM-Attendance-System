46afa29374e0024a799463b1afe6f4d35b7e493f
Fisseha W <fissehaweb@gmail.com>
2019-06-16 23:37:41 -0500

    Project Redesign

--- src/main/java/edu/mum/tmattendancesystem/repository/StudentRepository.java
+++ src/main/java/edu/mum/tmattendancesystem/repository/StudentRepository.java
@@ -1,9 +1,10 @@
 package edu.mum.tmattendancesystem.repository;
 
+import edu.mum.tmattendancesystem.domain.Student;
 import edu.mum.tmattendancesystem.domain.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface StudentRepository extends CrudRepository<User, String> {
+public interface StudentRepository extends CrudRepository<Student, String> {
 }
