ec0db6be55a0132839436f43b7c5b3615da1766a
Michael <mikialebachew@gmail.com>
2019-06-18 16:45:27 +0600

    File Upload insert to database

--- src/main/java/edu/mum/tmattendancesystem/controller/FileUploadController.java
+++ src/main/java/edu/mum/tmattendancesystem/controller/FileUploadController.java
@@ -1,5 +1,10 @@
 package edu.mum.tmattendancesystem.controller;
 
+import edu.mum.tmattendancesystem.domain.AttendanceKey;
+import edu.mum.tmattendancesystem.domain.MeditationTimeType;
+import edu.mum.tmattendancesystem.domain.TMAttendance;
+import edu.mum.tmattendancesystem.service.TMAttendanceService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,10 +17,14 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
 public class FileUploadController {
+
+    @Autowired
+    TMAttendanceService tmAttendanceService;
 
     //Save the uploaded file to this folder
     private static final String UPLOAD_LOCATION = "C://";
@@ -35,28 +44,10 @@ public class FileUploadController {
             return "redirect:/";
         }
 
-        try{
+        redirectAttributes.addFlashAttribute("uploadMessage",
+                "You successfully uploaded '" + file.getOriginalFilename() + "'");
 
-            // Get the file and save it somewhere
-            byte[] bytes = file.getBytes();
-            Path path = Paths.get(UPLOAD_LOCATION + file.getOriginalFilename());
-            Files.write(path, bytes);
-
-            List<String> allLines = Files.readAllLines(path, StandardCharsets.UTF_8);
-            for(String x : allLines){
-                String[] arrayString = x.split(",");
-
-                for(int i = 0; i < arrayString.length; i++){
-                    System.out.print(arrayString[i] + " - ");
-                }
-                System.out.println();
-            }
-
-            redirectAttributes.addFlashAttribute("uploadMessage",
-                    "You successfully uploaded '" + file.getOriginalFilename() + "'");
-        }catch(IOException io){
-            io.printStackTrace();
-        }
+        processFile(file);
 
         return "redirect:/";
     }
@@ -66,4 +57,69 @@ public class FileUploadController {
         return "dash";
     }
 
+    private void processFile(MultipartFile file){
+
+        try{
+
+            // Get the file and save it somewhere
+            byte[] bytes = file.getBytes();
+            Path path = Paths.get(UPLOAD_LOCATION + file.getOriginalFilename());
+            Files.write(path, bytes);
+
+            List<String> allLines = Files.readAllLines(path, StandardCharsets.UTF_8);
+            parseEachLineToModel(allLines);
+
+
+        }catch(IOException io){
+            io.printStackTrace();
+        }
+
+    }
+
+    private void printFileContentToConsole(List<String> allLines){
+        for(String x : allLines){
+            String[] arrayString = x.split(",");
+
+            for(int i = 0; i < arrayString.length; i++){
+                System.out.print(arrayString[i] + " - ");
+            }
+            System.out.println();
+        }
+    }
+
+    private void parseEachLineToModel(List<String> allLines){
+    System.out.println("ddddd inside parse each line to model");
+        for(String x : allLines){
+            String[] arrayString = x.split(",");
+            String[] date = arrayString[0].split("-");
+
+            TMAttendance tmAttendance = new TMAttendance();
+
+            if(arrayString.length == 5){
+
+                tmAttendance.setAttendanceKey(new AttendanceKey(
+                        arrayString[1],
+                        LocalDate.of(Integer.parseInt(date[0]),
+                                     Integer.parseInt(date[1]),
+                                     Integer.parseInt(date[2]))));
+                tmAttendance.setBarcode(arrayString[2]);
+                tmAttendance.setLocation(arrayString[3]);
+                tmAttendance.setMeditationTimeType(MeditationTimeType.getMeditationTimeType(arrayString[4]));
+
+
+                System.out.println("/// " + tmAttendance.toString());
+                tmAttendanceService.save(tmAttendance);
+
+            }else if(arrayString.length == 4){
+
+
+
+            }else{
+
+            }
+
+            System.out.println();
+        }
+
+    }
 }
--- src/main/java/edu/mum/tmattendancesystem/service/impl/TMAttendanceServiceImpl.java
+++ src/main/java/edu/mum/tmattendancesystem/service/impl/TMAttendanceServiceImpl.java
@@ -1,6 +1,21 @@
 package edu.mum.tmattendancesystem.service.impl;
 
+import edu.mum.tmattendancesystem.domain.TMAttendance;
+import edu.mum.tmattendancesystem.repository.TMAttendanceRepository;
 import edu.mum.tmattendancesystem.service.TMAttendanceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
 public class TMAttendanceServiceImpl implements TMAttendanceService {
+    @Autowired
+    TMAttendanceRepository tmAttendanceRepository;
+
+    @Override
+    public TMAttendance save(TMAttendance tmAttendance) {
+        return tmAttendanceRepository.save(tmAttendance);
+    }
 }
