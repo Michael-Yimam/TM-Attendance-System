0ae01096c9246ca759c266b3deec704e28e31bcf
Fisseha W <fissehaweb@gmail.com>
2019-06-18 10:40:13 -0500

    Login controller tested User populater added

---
+++ src/main/java/edu/mum/tmattendancesystem/controller/AdminLoginController.java
@@ -0,0 +1,82 @@
+package edu.mum.tmattendancesystem.controller;
+
+import javax.validation.Valid;
+
+import edu.mum.tmattendancesystem.domain.Role;
+import edu.mum.tmattendancesystem.domain.UserCredentials;
+import edu.mum.tmattendancesystem.service.RoleService;
+import edu.mum.tmattendancesystem.service.UserCredentialsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+public class AdminLoginController {
+
+    @Autowired
+    private UserCredentialsService userService;
+
+    @Autowired
+    private RoleService roleService;
+
+    @RequestMapping(value={"/", "/login"}, method = RequestMethod.GET)
+    public ModelAndView login(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("login");
+        return modelAndView;
+    }
+
+
+    @RequestMapping(value="/registration", method = RequestMethod.GET)
+    public ModelAndView registration(){
+        ModelAndView modelAndView = new ModelAndView();
+        UserCredentials user = new UserCredentials();
+        modelAndView.addObject("user", user);
+        modelAndView.setViewName("registration");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/registration", method = RequestMethod.POST)
+    public ModelAndView createNewUser(@Valid UserCredentials user, BindingResult bindingResult) {
+
+        System.out.println("Selected role: " + user.getRole());
+        ModelAndView modelAndView = new ModelAndView();
+
+        UserCredentials userExists = userService.findByUserName(user.getUserName());
+        if (userExists != null) {
+            bindingResult
+                    .rejectValue("userName", "error.admin",
+                            "There is already a admin registered with the userName provided");
+        }
+        if (bindingResult.hasErrors()) {
+            modelAndView.setViewName("registration");
+        } else {
+            userService.saveUserCredentials(user);
+            modelAndView.addObject("successMessage", "User has been registered successfully");
+            modelAndView.addObject("user", new UserCredentials());
+            modelAndView.setViewName("registration");
+
+        }
+        return modelAndView;
+    }
+
+    @RequestMapping(value="/admin/home", method = RequestMethod.GET)
+    public ModelAndView home(){
+        ModelAndView modelAndView = new ModelAndView();
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        UserCredentials user = userService.findByUserName(auth.getName());
+        //modelAndView.addObject("userName", "Welcome " + admin.getName() + " " + admin.getLastName() + " (" + admin.getEmail() + ")");
+        modelAndView.addObject("adminMessage","Content Available Only for Users with Admin Role");
+        modelAndView.setViewName("admin/home");
+        return modelAndView;
+    }
+
+}
+
